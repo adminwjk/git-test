d392a4c5e33a8a5e0f53a9c70a1758c3d7d38559
adminwjk <adminwjk@163.com>
2022-10-13 22:16:29 +0800

    删除线-----我删除一个文件

--- src/main/java/com/asiainfo/service/demo/GitTest.java
+++
@@ -1,9 +0,0 @@
-package com.asiainfo.service.demo;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("你好 adminwjk");
-        System.out.println("你好 adminwjk 123");
-        System.out.println("你好 adminwjk 1234");
-    }
-}
