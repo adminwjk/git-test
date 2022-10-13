d43703ac94143af1822d727c5c51db203eb1cd1d
adminwjk <adminwjk@163.com>
2022-10-13 22:15:45 +0800

    我再IDEA提交了 2

--- src/main/java/com/asiainfo/service/demo/GitTest.java
+++ src/main/java/com/asiainfo/service/demo/GitTest.java
@@ -3,5 +3,6 @@ package com.asiainfo.service.demo;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好 adminwjk");
+        System.out.println("你好 adminwjk 123");
     }
 }
