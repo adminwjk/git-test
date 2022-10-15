9b2dbdeb5261ceba854a53177a882e5186160057
adminwjk <adminwjk@163.com>
2022-10-15 09:28:01 +0800

    第二次提交test02

--- src/main/java/com/asiainfo/service/demo/GitTest.java
+++ src/main/java/com/asiainfo/service/demo/GitTest.java
@@ -3,9 +3,14 @@ package com.asiainfo.service.demo;
 public class GitTest {
     public static void main(String[] args) {
         test01();
+        test02();
     }
 
     private static void test01() {
         System.out.println("GitTest.test01");
+    }
+
+    private static void test02() {
+        System.out.println("GitTest.test02");
     }
 }
