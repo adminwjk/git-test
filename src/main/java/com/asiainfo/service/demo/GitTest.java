e49aa6df542ee9345cfe86546b0a2e55a00ffd24
adminwjk <adminwjk@163.com>
2022-10-15 09:27:29 +0800

    第一次提交test01

---
+++ src/main/java/com/asiainfo/service/demo/GitTest.java
@@ -0,0 +1,11 @@
+package com.asiainfo.service.demo;
+
+public class GitTest {
+    public static void main(String[] args) {
+        test01();
+    }
+
+    private static void test01() {
+        System.out.println("GitTest.test01");
+    }
+}
